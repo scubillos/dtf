e7d9fb35255407e1fcaae4197467f38600f2b4d7
scubillos <cbasqbll95@gmail.com>
2022-09-11 18:03:51 -0500

    ngrok

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://d645-3-94-97-60.ngrok.io/';
+//$config['base_url'] = 'https://d645-3-94-97-60.ngrok.io/';
+$config['base_url'] = "https://" . $_SERVER["SERVER_NAME"] . "/";
 
 /*
 |--------------------------------------------------------------------------
