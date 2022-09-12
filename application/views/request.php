975adecd30a964916738843065ba199e3fb2e528
scubillos <cbasqbll95@gmail.com>
2022-09-12 00:16:17 -0500

    dtf

--- application/views/request.php
+++ application/views/request.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title> Calculadora de IMC </title>
+        <title> Crear solicitud </title>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" />
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap-social.css') ?>" />
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/custom.css') ?>" />
@@ -10,13 +10,13 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-secondary" style="background-color:#23345a !important;">
-            <a class="navbar-brand" href="#">Calculadora de IMC</a>
+            <a class="navbar-brand" href="#">Crear solicitud</a>
             <div class="mx-auto"></div>
             <div class="nav navbar-nav navbar-right">
                 <a href="<?php echo base_url('User/logout') ?>" class="btn btn-outline-danger bg-light"> <i class="fas fa-sign-out-alt"></i> Cerrar Sesion</a>
             </div>
         </nav>
-        <div class="container" id="imc_home">
+        <div class="container" id="request_form">
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 text-center">
                     <h4>Bienvenid@ <?php echo $nombre_completo ?></h4>
@@ -26,60 +26,61 @@
                 <div class="col-md-12 col-sm-12 col-lg-8 mx-auto">
                     <div class="card">
                         <div class="card-header">
-                            Calcula tu Indice de Masa Corporal
+                            <i class="" />
+                            Crea una nueva solicitud de cambio
                         </div>
                         <div class="card-body">
                             <form>
                                 <div class="form-group row">
-                                    <label class="col-md-2">Peso</label>
-                                    <div class="input-group col-md-10">
-                                        <input type="number" step="any" min="0" class="form-control" v-model="imc.peso">
-                                        <div class="input-group-prepend">
-                                            <div class="input-group-text">kg</div>
-                                        </div>
+                                    <label class="col-md-4"># Factura</label>
+                                    <div class="input-group col-md-8">
+                                        <input type="number" step="any" min="0" class="form-control" v-model="request.id_factura">
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label class="col-md-2">Estatura</label>
-                                    <div class="input-group col-md-10">
-                                        <input type="number" min="0" class="form-control" v-model="imc.estatura">
-                                        <div class="input-group-prepend">
-                                            <div class="input-group-text">cm</div>
-                                        </div>
+                                    <label class="col-md-4">Tipo documento</label>
+                                    <div class="input-group col-md-8">
+                                        <select v-model="request.tipo_documento" class="form-control" required>
+                                            <option value="">Seleccione</option>
+                                            <option v-for="documento in documentos" v-value="documento.value">{{documento.text}}</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4"># Documento</label>
+                                    <div class="input-group col-md-8">
+                                        <input type="number" min="0" class="form-control" v-model="request.num_documento">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4">Producto</label>
+                                    <div class="input-group col-md-8">
+                                        <select v-model="request.id_producto" class="form-control" required>
+                                            <option value="">Seleccione</option>
+                                            <option v-for="producto in productos" v-value="producto.value">{{producto.text}}</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4">Email</label>
+                                    <div class="input-group col-md-8">
+                                        <input type="email" class="form-control" v-model="request.email">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-md-4">Observaciones</label>
+                                    <div class="input-group col-md-8">
+                                        <textarea class="form-control" v-model="request.num_documento" ></textarea>
                                     </div>
                                 </div>
                                 <div class="form-group row">
                                     <div class="col">
-                                        <button type="button" class="btn btn-primary" @click="registraImc()"><i class="fas fa-calculator"></i> Calcular</button>
+                                        <button type="button" class="btn btn-primary" @click="registraImc()"><i class="fas fa-save"></i> Guardar</button>
                                     </div>
                                 </div>
                             </form>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <button type="button" class="btn btn-info mx-auto" @click="toggleHistorico()" v-html="btnHistorico"></button>
-                <!-- Tabla que muestra los registros de la base de datos -->
-                <div class="table-responsive col-md-11 col-lg-11 col-sm-12 mx-auto" v-if="historico">
-                    <table class="table table-striped table-bordered" id="table_imc">
-                        <thead>
-                            <th>Peso</th>
-                            <th>Estatura</th>
-                            <th>IMC Calculado</th>
-                            <th>Clasificación</th>
-                            <th>Fecha/Hora registro</th>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(imc,i) in imc_arr" :key="i">
-                                <td v-text="imc.peso+' kg'"></td>
-                                <td v-text="imc.estatura+' cm'"></td>
-                                <td v-text="imc.imc_calculado"></td>
-                                <td v-text="imc.tipo+'/'+imc.clasificacion"></td>
-                                <td v-text="imc.fecha_registro"></td>
-                            </tr>
-                        </tbody>
-                    </table>
                 </div>
             </div>
         </div>   
@@ -88,6 +89,6 @@
         <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
         <script src="<?php echo base_url('assets/js/vue.min.js') ?>"></script>
         <script src="<?php echo base_url('assets/js/vue-resource.min.js') ?>"></script>
-        <script src="<?php echo base_url('assets/js/imc_home.js') ?>"></script>
+        <script src="<?php echo base_url('assets/js/request.js') ?>"></script>
     </body>
 </html>
--- assets/js/request.js
+++ assets/js/request.js
@@ -1,8 +1,25 @@
-var home = new Vue({
-    el: '#home',
-    data: function() {
-        return {
-        };
+var request_form = new Vue({
+    el: '#request_form',
+    data: {
+        api: {
+            bonita: "",
+            crm: "",
+            erp: "",
+        },
+        request: {
+            id_factura: null,
+            tipo_documento: "",
+            num_documento: null,
+            id_producto: "",
+            email: null,
+            observaciones: null,
+        },
+        documentos: [
+            { value: "CC", text: "Cédula de ciudadanía (CC)" },
+            { value: "CE", text: "Cédula de extrangería (CE)" },
+            { value: "TI", text: "Tarjeta de identidad (TI)" },
+        ],
+        productos: []    
     },
     created: function() {
         
