da090c7cc605b05aa38ed16348e12c7a97f9e0f8
helbert.cubillos <helbert.cubillos@bitsamericas.com>
2022-09-12 21:35:02 -0500

    llamados a api crm y erp

--- application/views/request.php
+++ application/views/request.php
@@ -5,6 +5,7 @@
         <title> Crear solicitud </title>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" />
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap-social.css') ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/select2.min.css') ?>" />
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/custom.css') ?>" />
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     </head>
@@ -30,52 +31,50 @@
                             Crea una nueva solicitud de cambio
                         </div>
                         <div class="card-body">
-                            <form>
+                            <form @submit.prevent="registerRequest" >
                                 <div class="form-group row">
                                     <label class="col-md-4"># Factura</label>
                                     <div class="input-group col-md-8">
-                                        <input type="number" step="any" min="0" class="form-control" v-model="request.id_factura">
+                                        <input type="number" step="any" min="0" class="form-control" v-model="request.id_factura" required >
                                     </div>
                                 </div>
                                 <div class="form-group row">
                                     <label class="col-md-4">Tipo documento</label>
                                     <div class="input-group col-md-8">
-                                        <select v-model="request.tipo_documento" class="form-control" required>
-                                            <option value="">Seleccione</option>
-                                            <option v-for="documento in documentos" v-value="documento.value">{{documento.text}}</option>
-                                        </select>
+                                    <select2 :options="documentos" class="form-control" v-model="request.tipo_documento">
+                                            <option disabled value="0">Seleccione</option>
+                                        </select2>
                                     </div>
                                 </div>
                                 <div class="form-group row">
                                     <label class="col-md-4"># Documento</label>
                                     <div class="input-group col-md-8">
-                                        <input type="number" min="0" class="form-control" v-model="request.num_documento">
+                                        <input type="number" min="0" class="form-control" v-model="request.num_documento" required >
                                     </div>
                                 </div>
                                 <div class="form-group row">
                                     <label class="col-md-4">Producto</label>
                                     <div class="input-group col-md-8">
-                                        <select v-model="request.id_producto" class="form-control" required>
-                                            <option value="">Seleccione</option>
-                                            <option v-for="producto in productos" v-value="producto.value">{{producto.text}}</option>
-                                        </select>
+                                        <select2 :options="productos" class="form-control" v-model="request.id_producto">
+                                            <option disabled value="0">Seleccione</option>
+                                        </select2>
                                     </div>
                                 </div>
                                 <div class="form-group row">
                                     <label class="col-md-4">Email</label>
                                     <div class="input-group col-md-8">
-                                        <input type="email" class="form-control" v-model="request.email">
+                                        <input type="email" class="form-control" v-model="request.email" required>
                                     </div>
                                 </div>
                                 <div class="form-group row">
                                     <label class="col-md-4">Observaciones</label>
                                     <div class="input-group col-md-8">
-                                        <textarea class="form-control" v-model="request.num_documento" ></textarea>
+                                        <textarea class="form-control" v-model="request.observaciones" required ></textarea>
                                     </div>
                                 </div>
                                 <div class="form-group row">
                                     <div class="col">
-                                        <button type="button" class="btn btn-primary" @click="registraImc()"><i class="fas fa-save"></i> Guardar</button>
+                                        <button type="submit" class="btn btn-primary"><i class="fas fa-save"></i> Guardar</button>
                                     </div>
                                 </div>
                             </form>
@@ -89,6 +88,13 @@
         <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
         <script src="<?php echo base_url('assets/js/vue.min.js') ?>"></script>
         <script src="<?php echo base_url('assets/js/vue-resource.min.js') ?>"></script>
+        <script src="<?php echo base_url('assets/js/select2.min.js') ?>"></script>
         <script src="<?php echo base_url('assets/js/request.js') ?>"></script>
+
+        <script type="text/x-template" id="select2-template">
+            <select>
+                <slot></slot>
+            </select>
+        </script>
     </body>
 </html>
--- assets/js/request.js
+++ assets/js/request.js
@@ -2,9 +2,16 @@ var request_form = new Vue({
     el: '#request_form',
     data: {
         api: {
-            bonita: "",
-            crm: "",
-            erp: "",
+            bonita: {
+                url: "",
+            },
+            crm: {
+                url: "https://b24-gg0vby.bitrix24.es/rest/1/bdjck2qfho6x61cr/",
+            },
+            erp: {
+                url: "https://api.alegra.com/api/",
+                token: "Y2Eucmlvc0BqYXZlcmlhbmEuZWR1LmNvOjY2NWM5NmY3OWZjNTZkZjhlZmQw",
+            }
         },
         request: {
             id_factura: null,
@@ -15,16 +22,121 @@ var request_form = new Vue({
             observaciones: null,
         },
         documentos: [
-            { value: "CC", text: "Cédula de ciudadanía (CC)" },
-            { value: "CE", text: "Cédula de extrangería (CE)" },
-            { value: "TI", text: "Tarjeta de identidad (TI)" },
+            { id: "CC", text: "Cédula de ciudadanía (CC)" },
+            { id: "CE", text: "Cédula de extrangería (CE)" },
+            { id: "TI", text: "Tarjeta de identidad (TI)" },
         ],
         productos: []    
     },
     created: function() {
-        
+        this.getProductos();
     },
     methods: {
-        
+        getProductos() {
+            let self = this;
+            fetch(self.api.erp.url + 'v1/items/', {
+                method: 'GET',
+                cache: 'no-cache',
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': 'Basic ' + self.api.erp.token,
+                },
+            }).then(async (response) => {
+                let productos = await response.json();
+                for (let i = 0; i < productos.length; i++ ) {
+                    const producto = productos[i];
+                    self.productos.push({ id: producto.id, text: producto.name });
+                }
+            })
+        },
+        async registerRequest() {
+            if (await this.validateEmail()) {
+                alert("Ejecutar llamado API Bonita");
+            } else {
+                toastr.error("La dirección de correo no corresponde a ningún cliente de DTF")
+            }
+        },
+        async validateEmail() {
+            let self = this;
+            let filter = `?FILTER[EMAIL]=${this.request.email}&SELECT[]=NAME&SELECT[]=LAST_NAME&SELECT[]=EMAIL"`
+            let valid = false;
+            await fetch(self.api.crm.url + 'crm.contact.list.json' + filter, {
+                method: 'GET',
+                cache: 'no-cache'
+            }).then(async (response) => {
+                let res = await response.json();
+                if (res.total != 0 && res.result.length != 0) {
+                    valid = true;
+                }
+            });
+
+            return valid;
+        }
     }
  });
+
+
+ // Componente select2
+Vue.component('select2', {
+  props: ['options', 'value'],
+  template: '#select2-template',
+  mounted: function () {
+    var vm = this
+    $(this.$el)
+      // init select2
+      .select2({ data: this.options })
+      .val(this.value)
+      .trigger('change')
+      // emit event on change.
+      .on('change', function () {
+        vm.$emit('input', this.value)
+      })
+  },
+  watch: {
+    value: function (value) {
+      // update value
+      $(this.$el)
+      	.val(value)
+      	.trigger('change')
+    },
+    options: function (options) {
+      // update options
+      $(this.$el).empty().select2({ data: options })
+    }
+  },
+  destroyed: function () {
+    $(this.$el).off().select2('destroy')
+  }
+})
+
+Vue.component('select2', {
+    props: ['options', 'value'],
+    template: '#select2-template',
+    mounted: function () {
+      var vm = this
+      $(this.$el)
+        // init select2
+        .select2({ data: this.options })
+        .val(this.value)
+        .trigger('change')
+        // emit event on change.
+        .on('change', function () {
+          vm.$emit('input', this.value)
+        })
+    },
+    watch: {
+      value: function (value) {
+        // update value
+        $(this.$el)
+            .val(value)
+            .trigger('change')
+      },
+      options: function (options) {
+        // update options
+        $(this.$el).empty().select2({ data: options })
+      }
+    },
+    destroyed: function () {
+      $(this.$el).off().select2('destroy')
+    }
+});
